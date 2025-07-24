573c6063a21e79ded61dc429740cec7316619e6d
Niya <n.v.neykova@tilburguniversity.edu>
2025-07-24 20:18:41 +0200

    Add files via upload

---
+++ dl_assign.py
@@ -0,0 +1,5 @@
+train_dir = "/Users/niyaneykova/Downloads/archive/train"
+val_dir = "/Users/niyaneykova/Downloads/archive/valid"
+test_dir = "/Users/niyaneykova/Downloads/archive/test"
+
+print(train_dir)
